e43c49418a5b8984f0d181030597644bfb29bef2
Sevastyan Rakhimov <rakhimovse@yandex-team.ru>
2019-03-05 17:42:27 +0300

    AnswerOrPayBot: Выложил хэндлеры в отдельный файл

--- rakhimovse/settings.py
+++ rakhimovse/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rakhimovse.api.AnswerOrPayBot.apps.AnswerorpaybotConfig'
 ]
 
 MIDDLEWARE = [
